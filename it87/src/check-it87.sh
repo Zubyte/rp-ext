c877ff4d8da0812f32f4f16a8f9cb0e215cfeb7a
Zubyte <28754230+Zubyte@users.noreply.github.com>
2023-05-01 21:46:47 +0200

    Fix pretty much everything

--- it87/src/check-it87.sh
+++ it87/src/check-it87.sh
@@ -2,9 +2,14 @@
 # Checking modules is loaded
 #
 
+echo -n "Loading module hwmon-vid -> "
+if [ `/sbin/lsmod | grep -i hwmon-vid | wc -l` -gt 0 ] ; then
+        echo "Module hwmon-vid loaded succesfully"
+        else echo "Module hwmon-vid is not loaded"
+fi
 echo -n "Loading module it87 -> "
-
 if [ `/sbin/lsmod | grep -i it87 | wc -l` -gt 0 ] ; then
         echo "Module it87 loaded succesfully"
-        else echo "Module it87 is not loaded "
+        else echo "Module it87 is not loaded"
 fi
+
